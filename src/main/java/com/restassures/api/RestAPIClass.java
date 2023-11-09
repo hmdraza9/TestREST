46f60678a797e8fcb5feddf4a5192093b51e07a2
Hamid Raza <hraza@lululemon.com>
2023-11-09 12:07:53 +0530

    initial commit Test REST API

---
+++ src/main/java/com/restassures/api/RestAPIClass.java
@@ -0,0 +1,103 @@
+package com.restassures.api;
+//import org.testng.AssertJUnit;
+
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.given;
+
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+
+public class RestAPIClass {
+
+	public static void main(String[] args) {
+//		RestAPIClass.myFirstRESTMethod();
+		RestAPIClass.mySecondRESTMethod(0);
+	}
+
+	public static void myFirstRESTMethod() {
+
+		// TODO Auto-generated method stub
+		String baseURI = "http://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=439d4b804bc8187953eb36d2a8c26a02";
+		RestAssured.baseURI = baseURI;
+		Response resp = RestAssured.get();
+//		System.out.println("given().baseUri(baseURI).get(): "+given().baseUri(baseURI).get().);
+
+		int code = resp.getStatusCode();
+
+		String data = resp.asString();
+
+		System.out.println("The status code is :" + code);
+
+//		AssertJUnit.assertEquals(code, 200);
+
+		System.out.println("The data is " + data);
+
+		System.out.println("The response time is :" + resp.getTime());
+
+		System.out.println("contentType: " + resp.contentType());
+
+		System.out.println("getContentType: " + resp.getContentType());
+
+		System.out.println("getSessionId: " + resp.getSessionId());
+
+		System.out.println("getStatusLine: " + resp.getStatusLine());
+
+		System.out.println("getTime: " + resp.getTime());
+
+		System.out.println("prettyPrint: " + resp.prettyPrint());
+
+		System.out.println("headers: " + resp.headers());
+
+		System.out.println("header: " + resp.header("Connection"));
+
+	}
+
+	public static void mySecondRESTMethod(int count) {
+
+		// without query parameter
+		String baseURI1 = "http://demo.guru99.com/V4/sinkministatement.php?CUSTOMER_ID=68195&PASSWORD=1234!&Account_No=1";
+//
+//		given().when().get(baseURI).then().log().all();
+
+		// with query parameter
+		baseURI = "http://demo.guru99.com/V4/sinkministatement.php";
+		given().queryParam("CUSTOMER_ID", "68195").queryParam("PASSWORD", "1234!").queryParam("Account_No", "1").when()
+				.get(baseURI).then().log().body();
+
+//		System.out.println("Status code: " + given().when().get(baseURI).statusCode());
+//		System.out
+//				.println("Status code: " + given().when().get(baseURI).timeIn(TimeUnit.MILLISECONDS) + " milliseconds");
+//		System.out.println(given().when().get(baseURI).then().assertThat().statusCode(200));
+
+		for (int i = 1; i <= count; i++) {
+			System.out.print(i + ". Status code: " + given().when().get(baseURI).statusCode() + "; ");
+			System.out.println(
+					"Status code: " + given().when().get(baseURI).timeIn(TimeUnit.MILLISECONDS) + " milliseconds");
+
+		}
+
+		int sumAmount = 0;
+//		ArrayList<String> amountArr = given().when().get(baseURI1).then().extract().path("result.statements.AMOUNT");
+////										when().get(baseURI1).then().extract().path("result.stements.AMOUNT");
+//		for (String str : amountArr) {
+//
+//			sumAmount = sumAmount + Integer.valueOf(str);
+//
+//		}
+		System.out.println("Sum of amounts: " + sumAmount);
+		
+		Response resp = given().when().get(baseURI1);
+		System.out.println("resp.asString(): "+resp);
+		System.out.println("resp.getBody(): "+resp.getBody());
+		System.out.println("resp.print(): "+resp.print());
+		
+		JsonPath jpe = resp.jsonPath();
+		String amount = jpe.get("");
+		System.out.println("Amount : "+ amount);
+
+	}
+}
