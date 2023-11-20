8534a4e2af23993875c95a42e5f2a327a619c195
Hamid Raza <hmdraza9@gmail.com>
2023-11-21 02:53:47 +0530

    few changes

--- src/main/java/learn/udemy/site/udemyRest.java
+++ src/main/java/learn/udemy/site/udemyRest.java
@@ -3,19 +3,19 @@ package learn.udemy.site;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
-import com.restassures.api.payloads;
+import com.restassures.api.testDataPayloads;
 
 import groovy.lang.GroovyRuntimeException;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 
 public class udemyRest {
-	
+
 	public static final String baseURI = "https://rahulshettyacademy.com";
-	
+
 	public static final String mapKey = "qaclick123";
-	
-	public static String placeID ;
+
+	public static String placeID;
 
 	public static void main(String[] args) {
 		udemyRest.addPlace("OK");
@@ -25,132 +25,105 @@ public class udemyRest {
 			udemyRest.getPlace("29, side layout, cohen 09");
 		} catch (GroovyRuntimeException e) {
 
-			System.out.println("Record not found, Status code: "+e.getLocalizedMessage().split("\n")[1]);
-			
+			System.out.println("Record not found, Status code: " + e.getLocalizedMessage().split("\n")[1]);
+
 		}
 
 	}
-	
-	public static void addPlace(String toVerify) {
-		System.out.println(new Throwable() 
-                                      .getStackTrace()[0] 
-                                      .getMethodName());
 
+	public static void addPlace(String toVerify) {
+		System.out.println(new Throwable().getStackTrace()[0].getMethodName());
 
 		RestAssured.baseURI = baseURI;
 
 		System.out.println("***************Request starts:***************");
-		
-		
-			Response addPlaceResp =	given()
+
+		Response addPlaceResp = given()
 //				.log()
 //				.all()
 				.queryParam("key", mapKey)
 				.header("Content-Type", "application/json")
 				.urlEncodingEnabled(false)
-				.body(payloads.addPlaceBody)
+				.body(testDataPayloads.addPlaceBody)
 				.when()
-				.post(payloads.uriAdd);
-			
-			System.out.println("\n\n***************Response starts***************n\n\n");
-			
-			addPlaceResp.then()
+				.post(testDataPayloads.uriAddPlace);
+
+		System.out.println("\n\n***************Response starts***************n\n\n");
+
+		addPlaceResp.then()
 //				.log()
 //				.all()
-				.assertThat().statusCode(200).body("scope", equalTo("APP")).body("status", equalTo(toVerify));
-				
-				placeID = addPlaceResp.getBody().path("place_id");
-				
-				System.out.println("\n\nPlace ID: "+placeID);
-		
-		
+				.assertThat()
+				.statusCode(200)
+				.body("scope", equalTo("APP"))
+				.body("status", equalTo(toVerify));
 
-		System.out.println("addPlace.asString: "+addPlaceResp.asString());
+		placeID = addPlaceResp
+				.getBody()
+				.path("place_id");
+
+		System.out.println("\n\nPlace ID: " + placeID);
+
+		System.out.println("addPlace.asString: " + addPlaceResp.asString());
 
 //		System.out.println("addPlace.statusCode: "+addPlaceResp.statusCode());
 	}
 
-	
 	public static void deletePlace(String toVerify) {
-		System.out.println(new Throwable() 
-                .getStackTrace()[0] 
-                .getMethodName());
+		System.out.println(new Throwable().getStackTrace()[0].getMethodName());
 
 		RestAssured.baseURI = baseURI;
 
 		System.out.println("***************Request starts:***************");
 
-
-		Response deletePlaceResp =	given()
+		Response deletePlaceResp = given()
 //			.log()
 //			.all()
-			.queryParam("key", "qaclick123")
-			.header("Content-Type", "application/json")
-			.urlEncodingEnabled(false)
-			.body(payloads.deletePlaceBody.replace("$RunTimeVar1", placeID))
-			.when()
-			.post(payloads.uriDelete);
-		
-		System.out.println("\n\n***************Response starts***************n\n\n");
-		
-		System.out.println("deletePlaceResp.asString: "+deletePlaceResp.asString());
+				.queryParam("key", "qaclick123")
+				.header("Content-Type", "application/json")
+				.urlEncodingEnabled(false)
+				.body(testDataPayloads.deletePlaceBody.replace("$RunTimeVar1", placeID))
+				.when()
+				.post(testDataPayloads.uriDeletePlace);
 
-		
+		System.out.println("\n\n***************Response starts***************n\n\n");
+
+		System.out.println("deletePlaceResp.asString: " + deletePlaceResp.asString());
+
 		deletePlaceResp.then()
 //		.log()
 //		.all()
-		.assertThat().statusCode(200).body("status", equalTo(toVerify));
-		
+				.assertThat().statusCode(200).body("status", equalTo(toVerify));
+
 		System.out.println("***************Place deleted successfully!***************");
 
-		
-		
-		
 	}
 
 	public static void getPlace(String toVerify) {
-		System.out.println(new Throwable() 
-                .getStackTrace()[0] 
-                .getMethodName());
-		
+		System.out.println(new Throwable().getStackTrace()[0].getMethodName());
+
 		RestAssured.baseURI = baseURI;
 
 		System.out.println("***************Request starts:***************");
 
-
-		Response getPlaceResp =	given()
+		Response getPlaceResp = given()
 //			.log()
 //			.all()
-			.queryParam("key", "qaclick123")
-			.header("Content-Type", "application/json")
-			.queryParam("place_id", placeID)
-			.urlEncodingEnabled(false)
-			.when()
-			.get(payloads.uriGet);
-		
+				.queryParam("key", "qaclick123")
+				.header("Content-Type", "application/json")
+				.queryParam("place_id", placeID)
+				.urlEncodingEnabled(false)
+				.when()
+				.get(testDataPayloads.uriGetPlace);
+
 		System.out.println("\n\n***************Response starts***************n\n\n");
-		
-		System.out.println("getPlaceResp.asString: "+getPlaceResp.asString());
-		
+
+		System.out.println("getPlaceResp.asString: " + getPlaceResp.asString());
+
 		getPlaceResp.then()
 //		.log()
 //		.all()
-		.assertThat().statusCode(200).body("address", equalTo(toVerify));
+				.assertThat().statusCode(200).body("address", equalTo(toVerify));
 	}
-	
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
