c71a8654dac53acaec167ccf98b3f1941794c08a
Hamid Raza <hmdraza9@gmail.com>
2025-01-18 01:23:41 +0530

    Some sysout changes

--- src/main/java/test/oauth/REST/PayPalOAuth.java
+++ src/main/java/test/oauth/REST/PayPalOAuth.java
@@ -38,10 +38,13 @@ public class PayPalOAuth {
         FileReader fr = new FileReader(configPropertyFile);
         Properties pr = new Properties();
         pr.load(fr);
-        return pr.getProperty(prop);
+        String temp = pr.getProperty(prop);
+        System.out.println("Property value for prop: "+prop+" returned as: "+temp);
+        return temp;
     }
 
     private static void getCredentialsFromPasteBin(String url) {
+        System.out.println("Fetching credentials from PastBin...");
         // Make the HTTP GET request using RestAssured
         // The URL of the page to fetch
 
@@ -63,6 +66,7 @@ public class PayPalOAuth {
             creds = dataText.split(" ");
             CLIENT_ID = PasswordEncryption.decrypt(creds[0], creds[1]);
             CLIENT_SECRET = PasswordEncryption.decrypt(creds[2], creds[1]);
+            System.out.println("Credentials fetched from PasteBin...");
         } else {
             System.out.println("Element with class 'de1' not found.");
         }
@@ -70,7 +74,9 @@ public class PayPalOAuth {
 
 
     private static void setBaseURIForPayPal() throws IOException {
-        RestAssured.baseURI = getProp("paypalBaseUrl");
+        String temp = getProp("paypalBaseUrl");
+        System.out.println("pay pal base URI set to: "+temp);
+        RestAssured.baseURI = temp;
     }
 
     private static void getTokenFromPayPal() {
@@ -79,13 +85,18 @@ public class PayPalOAuth {
 
         Response tokenResponse = given().header("Content-Type", "application/x-www-form-urlencoded").auth().preemptive().basic(CLIENT_ID, CLIENT_SECRET).param("grant_type", "client_credentials").post("/v1/oauth2/token");
         access_token = new JsonPath(tokenResponse.asString()).getString("access_token");
+        System.out.println("Token request success?: "+(tokenResponse.statusCode()==200));
+        System.out.println("Token fetched: "+(access_token.length()>10));
     }
 
     private static void getInvoiceTemplateFromPayPal() {
 
+        System.out.println("Requesting Invoicing templates.");
         //------ get invoice templates
 
         Response invTemplateResponse = given().header("Content-Type", "application/json").header("Authorization", "Bearer " + access_token).get("/v2/invoicing/templates");
+
+        System.out.println("Invoicing Templates response success? :"+(invTemplateResponse.statusCode()==200));
 
         System.out.println("First address: " + (new JsonPath(invTemplateResponse.asString()).getString("addresses[0]")));
 
